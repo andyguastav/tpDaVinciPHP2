4db97830c3180a4e9caa51c634b8a0062a463195
Andres Guastavino <andres.guastavino@davinci.edu.ar>
2023-10-11 17:09:43 +0200

    Colocando etiqueta php solo cuando se necesita y no imprimiento todo con echo

--- montone-guastavino/tp-1/templates/cds-detail.php
+++ montone-guastavino/tp-1/templates/cds-detail.php
@@ -1,45 +1,47 @@
+<?php
+// Cargar la información de los CDs desde el archivo JSON
+$jsonFile = 'data/cds.json';
+$cdsData = json_decode(file_get_contents($jsonFile), true);
 
-    <main class ="bodyFondoCds">
+// Verifica si se ha proporcionado un ID de CD válido en la URL
+if (isset($_GET['cd_id'])) {
+    $cdId = intval($_GET['cd_id']);
+
+    // Busca el CD correspondiente en el arreglo de datos
+    $foundCd = null;
+    foreach ($cdsData as $cd) {
+        if ($cd['cd_id'] === $cdId) {
+            $foundCd = $cd;
+            break;
+        }
+    }
+}
+?>
+
+<main class="bodyFondoCds">
     <div class="cd-container">
         <?php
-         // Cargar la información de los CDs desde el archivo JSON
-            $jsonFile = 'data/cds.json';
-            $cdsData = json_decode(file_get_contents($jsonFile), true);
-        // Verifica si se ha proporcionado un ID de CD válido en la URL
-        if (isset($_GET['cd_id'])) {
-            $cdId = intval($_GET['cd_id']);
-            
-            // Busca el CD correspondiente en el arreglo de datos
-            $foundCd = null;
-            foreach ($cdsData as $cd) {
-                if ($cd['cd_id'] === $cdId) {
-                    $foundCd = $cd;
-                    break;
-                }
-            }
-
-            // Muestra los detalles del CD si se encontró
-            if ($foundCd) {
-                echo '<div class="cd-card-details">';
-                echo '<div class="cd-card-details-left">';
-                echo '<img src="' . $foundCd['imagen'] . '" alt="' . $foundCd['titulo'] . '">';
-                echo '<p><strong>Precio:</strong> ' . $foundCd['precio'] . '</p>';
-                echo '<a href="index.php?seccion=productos" class="cd-button">Volver</a>';
-                echo '</div>';
-                echo '<div class="cd-card-details-right">';
-                echo '<h2>' . $foundCd['titulo'] . '</h2>';
-                echo '<p><strong>Discográfica:</strong> ' . $foundCd['discografica'] . '</p>';
-                echo '<p><strong>Productor:</strong> ' . $foundCd['productor'] . '</p>';
-                echo '<p><strong>Género:</strong> ' . $foundCd['genero'] . '</p>';
-                echo '<p>' . $foundCd['texto'] . '</p>';
-                echo '</div>';
-                echo '</div>';
-            } else {
-                echo '<p>CD no encontrado</p>';
-            }
+        // Muestra los detalles del CD si se encontró
+        if (isset($foundCd)) {
+        ?>
+            <div class="cd-card-details">
+                <div class="cd-card-details-left">
+                    <img src="<?= $foundCd['imagen'] ?>" alt="<?= $foundCd['titulo'] ?>">
+                    <p><strong>Precio:</strong> <?= $foundCd['precio'] ?></p>
+                    <a href="index.php?seccion=productos" class="cd-button">Volver</a>
+                </div>
+                <div class="cd-card-details-right">
+                    <h2><?= $foundCd['titulo'] ?></h2>
+                    <p><strong>Discográfica:</strong> <?= $foundCd['discografica'] ?></p>
+                    <p><strong>Productor:</strong> <?= $foundCd['productor'] ?></p>
+                    <p><strong>Género:</strong> <?= $foundCd['genero'] ?></p>
+                    <p><?= $foundCd['texto'] ?></p>
+                </div>
+            </div>
+        <?php
         } else {
-            echo '<p>CD no especificado</p>';
+            echo '<p>CD no encontrado</p>';
         }
         ?>
     </div>
-    </main>
+</main>
--- montone-guastavino/tp-1/templates/cds-list.php
+++ montone-guastavino/tp-1/templates/cds-list.php
@@ -1,27 +1,30 @@
+<?php
+// Cargar la información de los CDs desde el archivo JSON
+$jsonFile = 'data/cds.json';
+$cdsData = json_decode(file_get_contents($jsonFile), true);
+?>
 
-    <main class="bodyFondoCds">
+<main class="bodyFondoCds">
     <div class="cd-container">
         <ul>
             <?php
-            // Cargar la información de los CDs desde el archivo JSON
-            $jsonFile = 'data/cds.json';
-            $cdsData = json_decode(file_get_contents($jsonFile), true);
-
             // Recorre los CDs y muestra la lista
             foreach ($cdsData as $cd) {
-                echo '<li class="cd-card">';
-                echo '<div>';
-                echo '<a href="index.php?seccion=detalle&cd_id=' . $cd['cd_id'] . '">';
-                echo '<img src="' . $cd['imagen'] . '" alt="' . '" class="cd-image">';
-                echo '<h2 class="cd-title">' . $cd['titulo'] . '</h2>';
-                echo '<p class="cd-sinopsis">' . $cd['sinopsis'] . '</p>';
-                echo '<p class="cd-precio">' . $cd['precio'] . '</p>';
-                echo '</a>';
-                echo '<a class="cd-button" href="index.php?seccion=detalle&cd_id=' . $cd['cd_id'] . '">Ver Detalle</a>';
-                echo '</div>';
-                echo '</li>';
+            ?>
+            <li class="cd-card">
+                <div>
+                    <a href="index.php?seccion=detalle&cd_id=<?= $cd['cd_id'] ?>">
+                        <img src="<?= $cd['imagen'] ?>" alt="<?= $cd['titulo'] ?>" class="cd-image">
+                        <h2 class="cd-title"><?= $cd['titulo'] ?></h2>
+                        <p class="cd-sinopsis"><?= $cd['sinopsis'] ?></p>
+                        <p class="cd-precio"><?= $cd['precio'] ?></p>
+                    </a>
+                    <a class="cd-button" href="index.php?seccion=detalle&cd_id=<?= $cd['cd_id'] ?>">Ver Detalle</a>
+                </div>
+            </li>
+            <?php
             }
             ?>
         </ul>
     </div>
-    </main>
+</main>
