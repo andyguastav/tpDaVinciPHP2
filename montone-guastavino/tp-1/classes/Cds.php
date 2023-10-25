97b69186d7dc2a1fcccf047dddf11e7068f5d5db
Andres Guastavino <andres.guastavino@davinci.edu.ar>
2023-10-25 16:36:14 +0200

    Eliminando constructor para poder utilizar el PDO en el fetch-class

--- montone-guastavino/tp-1/classes/Cds.php
+++ montone-guastavino/tp-1/classes/Cds.php
@@ -9,15 +9,5 @@ class Cds {
     public $productor;
     public $genero;
 
-    public function __construct($cd_id, $titulo, $sinopsis, $texto, $imagen, $discografica, $productor, $genero) {
-        $this->cd_id = $cd_id;
-        $this->titulo = $titulo;
-        $this->sinopsis = $sinopsis;
-        $this->texto = $texto;
-        $this->imagen = $imagen;
-        $this->discografica = $discografica;
-        $this->productor = $productor;
-        $this->genero = $genero;
-    }
 }
 ?>
